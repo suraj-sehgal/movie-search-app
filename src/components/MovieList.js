3209489aa4f0d4e4ea1dd94990f4e3edd2875844
Suraj Sehgal <surajsehgalynnus@gmail.com>
2024-07-02 01:00:25 +0530

    No Result Found

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -2,11 +2,13 @@ import React from 'react';
 import MovieCard from './MovieCard';
 
 const MovieList = ({ movies }) => {
+  console.log(movies)
   return (
     <div className="h-full  w-full grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 justify-center gap-4 py-10" >
       {movies.map(movie => (
         <MovieCard key={movie.imdbID} movie={movie} />
       ))}
+      {movies.length===0&&<h1 className='text-white text-center'>No Result Found</h1>}
     </div>
   );
 };
